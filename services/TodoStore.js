5f5e5c77a4805d3877e173d2d15b081a7095c18b
Zeeshan Hassan <zishon89us@hotmail.com>
2015-04-04 19:34:16 +0500

    todo app

--- services/TodoStore.js
+++ services/TodoStore.js
@@ -1,3 +1,12 @@
 /**
  * Created by Zeeshan on 4/4/2015.
  */
+
+export class TodoStore {
+    constructor(){
+        this.todoList = [];
+    }
+    add(item){
+        this.todoList.unshift({text:item,done:false});
+    }
+}
