2c7ad5f8bb504f55280476b8ff4ba304b6e5ad89
Zeeshan Hassan <zishon89us@hotmail.com>
2015-04-06 19:48:01 +0500

    typescript init Fn Call

--- typescript/MSTutorial/myFile.ts
+++ typescript/MSTutorial/myFile.ts
@@ -1,3 +1,9 @@
 import {add, subtract} from "./math";
 import times from "math";
 var result = times(add(2, 3), subtract(5, 3));
+
+var myClient = {name: "Bob", height: 6};
+function greetClient({name, height: howTall}) {
+    console.log("Hello, " + name + ", who is " + howTall + " feet tall.");
+}
+greetClient(myClient);
